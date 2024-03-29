bfeb89921b8f9fb0d84dc0bd464db882947e1514
lamar <107258472+santospaul1@users.noreply.github.com>
2024-03-29 21:04:53 +0300

    dashboard

--- recursivedigitsum.py
+++ recursivedigitsum.py
@@ -1,53 +1,20 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'superDigit' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts following parameters:
-#  1. STRING n
-#  2. INTEGER k
-#
-
 def superDigit(n, k):
-    
+    # Calculate the super digit of a number
+    def calculate_super_digit(num):
+        # If the number has only one digit, return that digit
+        if len(num) == 1:
+            return int(num)
+        # Otherwise, sum the digits and recursively calculate the super digit
+        else:
+            digit_sum = sum(int(digit) for digit in num)
+            return calculate_super_digit(str(digit_sum))
+
+    # Concatenate n k times and calculate the super digit
     concatenated_n = n * k
-    
-    # Calculate the sum of digits of the concatenated string
-    digit_sum = sum(int(digit) for digit in concatenated_n)
-    
-    # If the sum has more than one digit, calculate the super digit recursively
-    while digit_sum >= 10:
-        digit_sum = sum(int(digit) for digit in str(digit_sum))
-    
-    return digit_sum
-  # Recursively calculate the super digit of the digit sum
-    #return superDigit(str(digit_sum), 1)
+    return calculate_super_digit(concatenated_n)
 
-    
-    
-                
 
-     
-    
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    first_multiple_input = input().rstrip().split()
-
-    n = first_multiple_input[0]
-
-    k = int(first_multiple_input[1])
-
-    result = superDigit(n, k)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+n = '1111'
+k = 4
+print(superDigit(n,k))
